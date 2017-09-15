a1f91116bf8e8b945222494ed8b40ab3489d14b4
Dharmesh Gohil <dgohil@groupon.com>
2017-09-14 23:31:10 -0700

    initial commit

---
+++ app/src/main/java/com/dharmesh/loginapp/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.dharmesh.loginapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+
+    public void login(View view) {
+        Toast.makeText(this, "Welcome to Instagram", Toast.LENGTH_LONG).show();
+    }
+}
